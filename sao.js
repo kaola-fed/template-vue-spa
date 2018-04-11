de54d5f8849f2e854d304ba71152e2c964974caf
吴子然 <wuziran@MacBook-Pro.local>
2018-04-11 14:17:50 +0800

    update outputPath choice

--- sao.js
+++ sao.js
@@ -8,6 +8,10 @@ module.exports = {
       message: `How would you descripe the new project?`,
       default: 'A awsome kaola project',
     },
+    outputPath: {
+      message: `Where do you want to output the dist files?`,
+      default: `./dist`
+    },
     owner: {
       message: `Who's the owner of the project?`,
       default: 'kaola-fed'
